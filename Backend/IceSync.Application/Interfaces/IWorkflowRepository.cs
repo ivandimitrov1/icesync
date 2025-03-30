48bf1a222a8579285a07cd1d619a9edc7c4fdaf1
Ivan Dimitrov <idimitrovpz@gmail.com>
2025-03-30 22:05:27 +0300

    - add tests

--- Backend/IceSync.Application/Interfaces/IWorkflowRepository.cs
+++ Backend/IceSync.Application/Interfaces/IWorkflowRepository.cs
@@ -6,12 +6,7 @@ namespace IceSync.Application.Interfaces;
 public interface IWorkflowRepository
 {
     Task<IList<Workflow>> GetAllByUserId(string userId);
-    Task<Workflow?> GetByIdAsync(int id);
-
-    Task CreateAsync(Workflow item);
-
     Task CreateAsync(IList<Workflow> items);
-    Task UpdateAsync(Workflow item);
     Task DeleteAsync(IList<Workflow> item);
     Task SaveAsync();
 }
--- Backend/IceSync.Infrastructure/Data/Repositories/WorkflowRepository.cs
+++ Backend/IceSync.Infrastructure/Data/Repositories/WorkflowRepository.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.EntityFrameworkCore;
 using IceSync.Application.Interfaces;
 using IceSync.Domain;
@@ -17,20 +16,8 @@ public class WorkflowRepository : IWorkflowRepository
     public async Task<IList<Workflow>> GetAllByUserId(string userId) =>
         await _context.Workflows.Where(x => x.UserId == userId).ToListAsync();
 
-    public async Task<Workflow?> GetByIdAsync(int id) => 
-        await _context.Workflows.FindAsync(id);
-
-    public async Task CreateAsync(Workflow item) => 
-        await _context.Workflows.AddAsync(item);
-
     public async Task CreateAsync(IList<Workflow> items) =>
         await _context.Workflows.AddRangeAsync(items);
-
-    public Task UpdateAsync(Workflow item)
-    {
-        _context.Entry(item).State = EntityState.Modified;
-        return Task.CompletedTask;
-    }
 
     public Task DeleteAsync(IList<Workflow> items)
     {
